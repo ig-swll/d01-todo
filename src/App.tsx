20651cbd133dad3049c27839a62c790c4aa601d8
Matheus Henrique <matheus.bezerra@senado.leg.br>
2022-11-08 11:16:14 -0300

    Desafio concluído

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,84 @@ import styles from './App.module.css';
 import { PlusCircle } from 'phosphor-react';
 import { Empty } from './components/Empty';
 import { Todo } from './components/Todo';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+interface Todo {
+  id: string;
+  done: boolean;
+  text: string;
+}
 
 export function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  function addTodo(text: string) {
+    const newTodo: Todo = {
+      id: uuidv4(),
+      text,
+      done: false,
+    };
+
+    setTodos([...todos, newTodo]);
+  }
+
+  function handleAddNewTodo(e: FormEvent) {
+    e.preventDefault();
+    if (newTodo.trim()) {
+      addTodo(newTodo);
+      setNewTodo('');
+    }
+  }
+
+  function handleNewTodoChange(e: ChangeEvent<HTMLInputElement>) {
+    e.target.setCustomValidity('');
+    setNewTodo(e.target.value);
+  }
+
+  function handleDeleteTodo(id: string) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function handleToggleDone(id: string) {
+    // const todoIdx = todos.findIndex((todo) => todo.id === id);
+    const modifiedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          done: !todo.done,
+        };
+      }
+      return todo;
+    });
+
+    setTodos(modifiedTodos);
+  }
+
+  function handleInvalidTodo(e: InvalidEvent<HTMLInputElement>) {
+    e.target.setCustomValidity('É obrigatório definir um texto!');
+  }
+
+  const doneTodos = todos.filter((todo) => todo.done);
+
   return (
     <>
       <header className={styles.topBackground}>
-        <img src={todoLogo} alt="Logo do ToDo" />
+        <a href="/">
+          <img src={todoLogo} alt="Logo do ToDo" />
+        </a>
       </header>
       <main className={styles.container}>
-        <form className={styles.inputForm}>
-          <input type="text" placeholder="Adicione uma nova tarefa" />
+        <form className={styles.inputForm} onSubmit={handleAddNewTodo}>
+          <input
+            type="text"
+            value={newTodo}
+            placeholder="Adicione uma nova tarefa"
+            onInvalid={handleInvalidTodo}
+            onChange={handleNewTodoChange}
+            required
+          />
           <button type="submit">
             Criar <PlusCircle size={16} weight="bold" />
           </button>
@@ -22,25 +90,32 @@ export function App() {
           <div className={styles.listHeader}>
             <div>
               <strong>Tarefas criadas</strong>
-              <span>0</span>
+              <span>{todos.length}</span>
             </div>
 
             <div>
               <strong>Concluídas</strong>
-              <span>0</span>
+              <span>
+                {todos.length ? `${doneTodos.length} de ${todos.length}` : 0}
+              </span>
             </div>
           </div>
 
           <div className={styles.list}>
-            {/* <Empty /> */}
-            <Todo
-              done={false}
-              text="Lorem ipsum dolor sit amet consectetur adipiscing elit"
-            />
-            <Todo
-              done={true}
-              text="Lorem ipsum dolor sit amet consectetur adipiscing elit"
-            />
+            {todos.length ? (
+              todos.map((todo) => (
+                <Todo
+                  key={todo.id}
+                  id={todo.id}
+                  done={todo.done}
+                  text={todo.text}
+                  onDeleteTodo={handleDeleteTodo}
+                  onToggleDone={handleToggleDone}
+                />
+              ))
+            ) : (
+              <Empty />
+            )}
           </div>
         </div>
       </main>
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,17 +1,51 @@
+import { Trash } from 'phosphor-react';
+import { MouseEvent } from 'react';
 import styles from './Todo.module.css';
 
 interface TodoProps {
+  id: string;
   done: boolean;
   text: string;
-  toggleDone?: () => void;
-  deleteTodo?: () => void;
+  onToggleDone: (id: string) => void;
+  onDeleteTodo: (id: string) => void;
 }
 
-export function Todo({ done, text }: TodoProps) {
+export function Todo(props: TodoProps) {
+  const { id, done, text, onDeleteTodo, onToggleDone } = props;
+  const toggleDone = () => {
+    onToggleDone(id);
+  };
+  const deleteTodo = () => {
+    onDeleteTodo(id);
+  };
+
+  function textDoubleClick(event: MouseEvent<HTMLDivElement>) {
+    if (event.detail > 1) {
+      event.preventDefault();
+    }
+  }
+
   return (
-    <div className={styles.todo}>
-      <input type="checkbox" checked={done} />
-      <p>{text}</p>
+    <div className={done ? styles.todoDone : styles.todoNotDone}>
+      <div className={styles.todoContent}>
+        <div
+          className={styles.labelWrapper}
+          onClick={toggleDone}
+          onMouseDown={textDoubleClick}
+        >
+          <input
+            name="todo-checkbox"
+            type="checkbox"
+            checked={done}
+            onClick={toggleDone}
+            onChange={toggleDone}
+          />
+          <label htmlFor="todo-checkbox">{text}</label>
+        </div>
+        <button type="button" onClick={deleteTodo}>
+          <Trash size={14} />
+        </button>
+      </div>
     </div>
   );
 }
